286fffe2d7e1c82aafafe02dbf6a28180aad75b7
Itaï BEN YAACOV <80927047+begnac@users.noreply.github.com>
2023-04-13 10:22:22 +0200

    setup cleanup.

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 # coding: utf-8
 
-import setuptools
-from babel.messages import frontend as babel
+from setuptools import setup
 
-setuptools.setup(
-    cmdclass={'compile_catalog': babel.compile_catalog,
-              'extract_messages': babel.extract_messages,
-              'init_catalog': babel.init_catalog,
-              'update_catalog': babel.update_catalog},
-)
+setup()
