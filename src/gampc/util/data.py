ce553068dac2b3857adbf8012e452224caf3e0c1
Itaï BEN YAACOV <80927047+begnac@users.noreply.github.com>
2023-05-21 16:48:05 +0200

    Focus.

--- src/gampc/util/data.py
+++ src/gampc/util/data.py
@@ -495,12 +495,12 @@ class TreeViewFilter(Gtk.Box):
         self.filter_ = Record()
         self.filter_treeview = RecordTreeViewFilter(unit_misc, self.filter_, treeview.fields)
 
-        filter_scroller = Gtk.ScrolledWindow(visible=True)
+        filter_scroller = Gtk.ScrolledWindow(visible=True, can_focus=False)
         filter_scroller.add(self.filter_treeview)
         filter_scroller.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.NEVER)
         self.add(filter_scroller)
 
-        self.scroller = Gtk.ScrolledWindow(visible=True)
+        self.scroller = Gtk.ScrolledWindow(visible=True, can_focus=False)
         self.scroller.add(treeview)
         self.add(self.scroller)
         self.treeview = treeview
