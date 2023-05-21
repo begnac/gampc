a0abfc13156b526e947fee147dae05e024110a77
Itaï BEN YAACOV <80927047+begnac@users.noreply.github.com>
2023-05-21 14:43:31 +0200

    Current component cleanups.

--- src/gampc/components/current.py
+++ src/gampc/components/current.py
@@ -89,16 +89,15 @@ class Welcome(Gtk.Box):
 
 
 class Person(Gtk.Box):
-    name = GObject.Property(type=str)
-
     def __init__(self, image_cache, condition=None):
-        self.image_cache = image_cache
-        self.condition = condition
         super().__init__(visible=True, orientation=Gtk.Orientation.VERTICAL)
 
-        self.connect('notify::name', self.notify_name_cb)
+        self.image_cache = image_cache
+        self.condition = condition
 
         self.label = Gtk.Label(vexpand=True, ellipsize=Pango.EllipsizeMode.MIDDLE, lines=3, wrap=True)
+        self.label.connect('notify::label', self.notify_label_cb)
+
         self.image = Gtk.Image(vexpand=True)
         self.image_label = Gtk.Label()
 
@@ -112,24 +111,24 @@ class Person(Gtk.Box):
         self.image.clear()
         self.last_wh = None
 
-    def notify_name_cb(self, label, param):
+    def notify_label_cb(self, label, param):
         self.clear()
 
-        if not self.name or (self.condition and not self.condition(self.name)):
+        name = self.label.props.label
+        if not name or (self.condition and not self.condition(name)):
             self.set_visible(False)
             return
 
         self.set_visible(True)
 
-        self.pixbuf = self.image_cache[self.name]
+        self.pixbuf = self.image_cache[name]
         if self.pixbuf:
             self.label.set_visible(False)
             self.image.set_visible(True)
             self.image_label.set_visible(True)
-            self.image_label.set_label(self.name)
+            self.image_label.set_label(name)
         else:
             self.label.set_visible(True)
-            self.label.set_label(self.name)
             self.image.set_visible(False)
             self.image_label.set_visible(False)
 
@@ -147,6 +146,48 @@ class Person(Gtk.Box):
         image.set_from_pixbuf(pixbuf)
 
 
+class Info(Gtk.Box):
+    size = GObject.Property(type=int)
+
+    def __init__(self):
+        super().__init__(hexpand=True, orientation=Gtk.Orientation.VERTICAL, homogeneous=True)
+        self.pixbufs = PixbufCache()
+
+        self.artist = Person(self.pixbufs)
+        self.artist.label.set_attributes(Pango.AttrList.from_string('0 -1 font-desc "Serif Bold", 0 -1 scale 2'))
+
+        self.performer = Person(self.pixbufs, lambda name: name != 'Instrumental')
+        self.performer.label.set_attributes(Pango.AttrList.from_string('0 -1 font-desc "Sans", 0 -1 scale 2'))
+
+        artist_performer_box = Gtk.Box(visible=True, vexpand=True, homogeneous=True, spacing=50)
+        artist_performer_box.add(self.artist)
+        artist_performer_box.add(self.performer)
+
+        self.title_label = Gtk.Label(visible=True, vexpand=True, ellipsize=Pango.EllipsizeMode.MIDDLE)
+        self.title_label.set_attributes(Pango.AttrList.from_string('0 -1 font-desc "Sans Bold Italic", 0 -1 scale 3'))
+
+        self.genre_label = Gtk.Label(visible=True)
+        self.date_label = Gtk.Label(visible=True)
+        self.composer_label = Gtk.Label(visible=True)
+        data_box = Gtk.Box(visible=True, vexpand=True, halign=Gtk.Align.CENTER, spacing=14)
+        data_box.add(self.genre_label)
+        data_box.add(Gtk.Label(visible=True, label="/"))
+        data_box.add(self.date_label)
+        data_box.add(Gtk.Label(visible=True, label="/"))
+        data_box.add(self.composer_label)
+
+        info_box = Gtk.Box(visible=True, vexpand=True, orientation=Gtk.Orientation.VERTICAL)
+        info_box.add(self.title_label)
+        info_box.add(data_box)
+
+        self.add(artist_performer_box)
+        self.add(info_box)
+
+    def clear(self):
+        self.artist.clear()
+        self.performer.clear()
+
+
 class Current(component.Component):
     size = GObject.Property(type=int)
 
@@ -154,58 +195,27 @@ class Current(component.Component):
         super().__init__(*args)
         self.window_signals['check-resize'] = self.window_check_resize_cb
 
-        self.pixbufs = PixbufCache()
-
         self.labels = {}
-        self.images = {}
-        self.image_labels = {}
 
         welcome = Welcome()
+        self.info = Info()
 
-        self.artist = Person(self.pixbufs)
-        self.unit.unit_server.bind_property('current-song', self.artist, 'name',
-                                            GObject.BindingFlags.SYNC_CREATE,
-                                            lambda x, y: y.get('Artist', ''))
-        self.artist.label.set_attributes(Pango.AttrList.from_string('0 -1 font-desc "Serif Bold", 0 -1 scale 2'))
-
-        self.performer = Person(self.pixbufs, lambda name: name != 'Instrumental')
-        self.unit.unit_server.bind_property('current-song', self.performer, 'name',
-                                            GObject.BindingFlags.SYNC_CREATE,
-                                            lambda x, y: y.get('Performer', ''))
-        self.performer.label.set_attributes(Pango.AttrList.from_string('0 -1 font-desc "Sans", 0 -1 scale 2'))
-
-        artist_performer_box = Gtk.Box(visible=True, vexpand=True, homogeneous=True, spacing=50)
-        artist_performer_box.add(self.artist)
-        artist_performer_box.add(self.performer)
-
-        self.labels['Title'] = title_label = Gtk.Label(visible=True, vexpand=True, ellipsize=Pango.EllipsizeMode.MIDDLE)
-        title_label.set_attributes(Pango.AttrList.from_string('0 -1 font-desc "Sans Bold Italic", 0 -1 scale 3'))
-
-        self.labels['Genre'] = genre_label = Gtk.Label(visible=True)
-        self.labels['Date'] = date_label = Gtk.Label(visible=True)
-        self.labels['Composer'] = composer_label = Gtk.Label(visible=True)
-        data_box = Gtk.Box(visible=True, vexpand=True, halign=Gtk.Align.CENTER, spacing=14)
-        data_box.add(genre_label)
-        data_box.add(Gtk.Label(visible=True, label="/"))
-        data_box.add(date_label)
-        data_box.add(Gtk.Label(visible=True, label="/"))
-        data_box.add(composer_label)
-
-        info_box = Gtk.Box(visible=True, vexpand=True, orientation=Gtk.Orientation.VERTICAL)
-        info_box.add(title_label)
-        info_box.add(data_box)
-
-        self.current = Gtk.Box(hexpand=True, orientation=Gtk.Orientation.VERTICAL, homogeneous=True)
-        self.current.add(artist_performer_box)
-        self.current.add(info_box)
+        self.labels = (
+            ('Artist', self.info.artist.label),
+            ('Performer', self.info.performer.label),
+            ('Title', self.info.title_label),
+            ('Genre', self.info.genre_label),
+            ('Date', self.info.date_label),
+            ('Composer', self.info.composer_label),
+        )
 
         self.widget = self.main_box = Gtk.Box(visible=True, margin_bottom=20, margin_left=20, margin_right=20, margin_top=20)
         self.main_box.add(welcome)
-        self.main_box.add(self.current)
+        self.main_box.add(self.info)
 
         self.unit.unit_server.bind_property('current-song', welcome, 'visible', GObject.BindingFlags.SYNC_CREATE, lambda x, y: not y)
-        self.unit.unit_server.bind_property('current-song', self.current, 'visible', GObject.BindingFlags.SYNC_CREATE, lambda x, y: bool(y))
-        self.signal_handler_connect(self.unit.unit_server, 'notify::current-song', self.fader)
+        self.unit.unit_server.bind_property('current-song', self.info, 'visible', GObject.BindingFlags.SYNC_CREATE, lambda x, y: bool(y))
+        self.signal_handler_connect(self.unit.unit_server, 'notify::current-song', self.notify_current_song_cb)
         self.fading = None
 
         self.width = 0
@@ -213,14 +223,6 @@ class Current(component.Component):
         self.widget.get_style_context().add_provider(self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
         self.widget.connect('size-allocate', self.size_allocate_cb)
         self.bind_property('size', welcome, 'size')
-
-        for field in self.labels.keys():
-            label = self.labels[field]
-            self.unit.unit_server.bind_property('current-song',
-                                                label, 'label',
-                                                GObject.BindingFlags.SYNC_CREATE,
-                                                lambda x, y, z: self.set_size() or y.get(z, ''),
-                                                None, field)
 
         self.widget.connect('map', self.__map_cb)
 
@@ -234,32 +236,37 @@ class Current(component.Component):
         self.width = 0
         self.set_size()
 
+    def notify_current_song_cb(self, server, param):
+        for field, label in self.labels:
+            label.set_label(server.current_song.get(field, ''))
+        self.set_size()
+        # self.fader()
+
     def window_check_resize_cb(self, win):
-        self.artist.clear()
-        self.performer.clear()
+        self.info.clear()
 
-    @ampd.task
-    async def fader(self, *args):
-        START = 30
-        DURATION = 3
-        INTERVAL = 0.05
+    # @ampd.task
+    # async def fader(self, *args):
+    #     START = 30
+    #     DURATION = 3
+    #     INTERVAL = 0.05
 
-        if self.fading:
-            self.fading.cancel()
-        task = self.fading = asyncio.current_task()
-        try:
-            if self.unit.unit_persistent.dark and self.unit.unit_server.current_song:
-                self.current.set_opacity(0)
-                await asyncio.sleep(START)
-                t0 = t1 = time.time()
-                while t1 < t0 + DURATION:
-                    self.current.set_opacity((t1 - t0) / DURATION)
-                    await asyncio.sleep(INTERVAL)
-                    t1 = time.time()
-            self.current.set_opacity(1)
-        finally:
-            if self.fading == task:
-                self.fading = None
+    #     if self.fading:
+    #         self.fading.cancel()
+    #     task = self.fading = asyncio.current_task()
+    #     try:
+    #         if self.unit.unit_persistent.dark and self.unit.unit_server.current_song:
+    #             self.info.set_opacity(0)
+    #             await asyncio.sleep(START)
+    #             t0 = t1 = time.time()
+    #             while t1 < t0 + DURATION:
+    #                 self.info.set_opacity((t1 - t0) / DURATION)
+    #                 await asyncio.sleep(INTERVAL)
+    #                 t1 = time.time()
+    #         self.info.set_opacity(1)
+    #     finally:
+    #         if self.fading == task:
+    #             self.fading = None
 
     def size_allocate_cb(self, widget, allocation):
         self.width = allocation.width
