da78ffd885debc24d18d34c026cdb08f2add57d1
Itaï BEN YAACOV <80927047+begnac@users.noreply.github.com>
2023-12-28 16:51:40 +0100

    CONNECT condition removed

--- src/gampc/units/server.py
+++ src/gampc/units/server.py
@@ -153,9 +153,8 @@ class __unit__(unit.UnitConfigMixin, unit.Unit):
 
     @ampd.task
     async def _reconnect(self):
-        reply = await self.ampd.idle(ampd.CONNECT, timeout=1)
-        if reply & ampd.TIMEOUT:
-            await self.ampd_client.connect_to_server(self.profile.address)
+        await asyncio.sleep(1)
+        await self.ampd_client.connect_to_server(self.profile.address)
 
     @ampd.task
     async def idle_database(self):
