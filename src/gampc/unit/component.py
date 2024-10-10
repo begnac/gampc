5c6b86a4de1248aff360f247911ebda5b8951c19
Itaï BEN YAACOV <80927047+begnac@users.noreply.github.com>
2024-10-10 17:22:31 +0200

    Better focus handling when changing component.

--- src/gampc/unit/component.py
+++ src/gampc/unit/component.py
@@ -116,7 +116,7 @@ class __unit__(unit.Unit):
             window = Gtk.Application.get_default().get_active_window()
             window.change_component(component)
         else:
-            window.component.grab_focus()
+            window.grab_focus()
         window.present()
 
     def component_stop_cb(self, action, parameter):
